94028027097269c08e580dac1c32c51da6843fdf
yuma.takushima <takushima@valuedesign.jp>
2024-04-30 15:48:30 +0900

    ログアウトアクションの作成

--- src/main/java/actions/AuthAction.java
+++ src/main/java/actions/AuthAction.java
@@ -87,4 +87,12 @@ public class AuthAction extends ActionBase{
         forward(ForwardConst.FW_LOGIN);
     }
 
+    public void logout() throws ServletException, IOException{
+        removeSessionScope(AttributeConst.LOGIN_EMP);
+
+        putSessionScope(AttributeConst.FLUSH, MessageConst.I_LOGOUT.getMessage());
+
+        redirect(ForwardConst.ACT_AUTH, ForwardConst.CMD_SHOW_LOGIN);
+    }
+
 }
