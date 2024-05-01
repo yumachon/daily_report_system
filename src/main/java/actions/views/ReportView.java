933811d39ce72c7d2e6ee434ad5fe8100ac8e129
yuma.takushima <takushima@valuedesign.jp>
2024-05-01 13:43:29 +0900

    Reportモデル、ReportViewモデル作成

---
+++ src/main/java/actions/views/ReportView.java
@@ -0,0 +1,39 @@
+package actions.views;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class ReportView {
+
+    //id
+    private Integer id;
+
+    //日報を登録した従業員
+    private EmployeeView employee;
+
+    //いつの日報かを示す日付
+    private LocalDate reportDate;
+
+    //日報のタイトル
+    private String title;
+
+    //日報の内容
+    private String content;
+
+    //登録日時
+    private LocalDateTime createdAt;
+
+    //更新日時
+    private LocalDateTime updatedAt;
+
+}
