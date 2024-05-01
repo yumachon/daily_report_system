211c7113e85f155ca6e47ddf81d5c1b746488a9d
yuma.takushima <takushima@valuedesign.jp>
2024-05-01 14:51:05 +0900

    ReportValidator、ReportService作成

---
+++ src/main/java/models/validators/ReportValidator.java
@@ -0,0 +1,46 @@
+package models.validators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import actions.views.ReportView;
+import constants.MessageConst;
+
+//日報インスタンスに設定されている値のバリデーションを行うクラス
+public class ReportValidator {
+
+    public static List<String> validate(ReportView rv){
+        List<String> errors = new ArrayList<String>();
+
+        //タイトルのチェック
+        String titleError = validateTitle(rv.getTitle());
+        if(!titleError.equals("")) {
+            errors.add(titleError);
+        }
+
+        //内容のチェック
+        String contentError = validateContent(rv.getContent());
+        if(!contentError.equals("")) {
+            errors.add(contentError);
+        }
+
+        return errors;
+    }
+
+    private static String validateTitle(String title) {
+        if(title == null || title.equals("")) {
+            return MessageConst.E_NOTITLE.getMessage();
+        }
+
+        return "";
+    }
+
+    private static String validateContent(String content) {
+        if(content == null || content.equals("")) {
+            return MessageConst.E_NOCONTENT.getMessage();
+        }
+
+        return "";
+    }
+
+}
