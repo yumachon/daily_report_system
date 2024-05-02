b0fd3b1bad3d08f5e8b266425546af3963863c3c
yuma.takushima <takushima@valuedesign.jp>
2024-05-02 10:22:10 +0900

    トップページアクションの作成

--- src/main/java/actions/TopAction.java
+++ src/main/java/actions/TopAction.java
@@ -1,27 +1,51 @@
 package actions;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 
+import actions.views.EmployeeView;
+import actions.views.ReportView;
 import constants.AttributeConst;
 import constants.ForwardConst;
+import constants.JpaConst;
+import services.ReportService;
 
 public class TopAction extends ActionBase {
 
+    private ReportService service;
+
     @Override
     public void process() throws ServletException, IOException{
+        service = new ReportService();
+
         invoke();
+
+        service.close();
     }
 
     public void index() throws ServletException, IOException{
-         String flush = getSessionScope(AttributeConst.FLUSH);
-         if(flush != null) {
-             putRequestScope(AttributeConst.FLUSH, flush);
-             removeSessionScope(AttributeConst.FLUSH);
-         }
+        EmployeeView loginEmployee =(EmployeeView) getSessionScope(AttributeConst.LOGIN_EMP);
 
-         forward(ForwardConst.FW_TOP_INDEX);
+        int page = getPage();
+        List<ReportView> reports = service.getMinePerPage(loginEmployee, page);
+
+        //ログイン中の従業員が作成した日報データの件数を取得
+        long myReportsCount = service.countAllMine(loginEmployee);
+
+        putRequestScope(AttributeConst.REPORTS, reports); //取得した日報データ
+        putRequestScope(AttributeConst.REP_COUNT, myReportsCount); //ログイン中の従業員が作成した日報の数
+        putRequestScope(AttributeConst.PAGE, page); //ページ数
+        putRequestScope(AttributeConst.MAX_ROW, JpaConst.ROW_PER_PAGE); //1ページに表示するレコードの数
+
+        String flush = getSessionScope(AttributeConst.FLUSH);
+        if(flush != null) {
+            putRequestScope(AttributeConst.FLUSH, flush);
+            removeSessionScope(AttributeConst.FLUSH);
+        }
+
+        forward(ForwardConst.FW_TOP_INDEX);
     }
 
 }
